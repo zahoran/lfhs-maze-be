72fd3a1d1fc3e58ee8ec259c8fdeeb81cf6be562
Daniel Zahoran <zahoran.daniel@wysio.hu>
2020-07-16 13:48:38 +0200

    remove localhost refence for heroku

--- client/src/service.js
+++ client/src/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const solveMaze = (maze) => {
-  return axios.post('http://localhost:5000/test', maze).then(response => {
+  return axios.post('/test', maze).then(response => {
     console.log('response');
     return response.data.solution;
   }).catch(error => {
